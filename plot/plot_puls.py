d489edb6eeb65811ca3eb470f4c58e51c6ffc4f9
TLRKiliann <philogenie@protonmail.com>
2020-03-23 15:24:53 +0100

    add plot

---
+++ plot/plot_puls.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+import json
+import matplotlib.pyplot as plt
+import os
+
+
+print("\nListe1 = dates :")
+print("--------------")
+fileO = open("data_datepuls.json")
+list1 = json.load(fileO)
+#f.close
+
+for letter in list1:
+    print("list1: " + letter)
+
+print("\nList2 = Puls :")
+print("--------------------")
+
+fileO = open("data_puls.json")
+list2 = json.load(fileO)
+#f.close
+
+for letter in list2:
+    print("List2: " + letter)
+
+print("\nSuper LOOP!\n")
+
+dicolist = {}
+
+for list1, list2 in zip(list1, list2):
+    dicolist[list1] = list2
+
+print("Affichage du dictionnaire :")
+print("---------------------------")
+print(dicolist)
+
+list1 = []
+list2 = []
+
+for key, value in dicolist.items():
+    list1.append(key)
+    list2.append(value)
+    
+print("\nListe des dates dans l'ordre des entrées :")
+print("----------------------------------")
+print(list1)
+# How to sort data of list1 to correspond whith list2 ???
+# And for list2 ??? Is it with array ??? 
+print("\nListe des pulsations :")
+print("------------------------")
+print(list2)
+
+#list3 = [int(list2) for list2 in list2]
+list2 = list(map(int, list2))
+
+show_grid = True
+with plt.style.context(('seaborn-darkgrid')):
+    plt.plot(list1, list2)
+    plt.ylabel('Puls/min')
+    plt.xlabel('Dates')
+    plt.title('Relevé des puls/min par date')
+    plt.xticks(rotation=45)
+    plt.grid(show_grid)
+    plt.show()
+
+os.remove("data_datepuls.json")
+print("File data_datepuls.json has removed")
+os.remove("data_puls.json")
+print("File data_puls.json has removed")
---
+++ plot/plot_temp.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+import json
+import matplotlib.pyplot as plt
+import os
+
+
+print("\nListe1 = dates :")
+print("--------------")
+fileO = open("data_datetemp.json")
+list1 = json.load(fileO)
+#f.close
+
+for letter in list1:
+    print("list1: " + letter)
+
+print("\nList2 = temperatures :")
+print("--------------------")
+
+fileO = open("data_temp.json")
+list2 = json.load(fileO)
+#f.close
+
+for letter in list2:
+    print("List2: " + letter)
+
+print("\nSuper LOOP!\n")
+
+dicolist = {}
+
+for list1, list2 in zip(list1, list2):
+    dicolist[list1] = list2
+
+print("Affichage du dictionnaire :")
+print("---------------------------")
+print(dicolist)
+
+list1 = []
+list2 = []
+
+for key, value in dicolist.items():
+    list1.append(key)
+    list2.append(value)
+    
+print("\nListe des dates dans l'ordre des entrées :")
+print("----------------------------------")
+print(list1)
+# How to sort data of list1 to correspond whith list2 ???
+# And for list2 ??? Is it with array ??? 
+print("\nList of temperatures :")
+print("------------------------")
+print(list2)
+
+#list3 = [int(list2) for list2 in list2]
+list2 = list(map(float, list2))
+
+show_grid = True
+with plt.style.context(('seaborn-darkgrid')):
+    plt.plot(list1, list2)
+    plt.ylabel('T°C')
+    plt.xlabel('Dates')
+    plt.title('Relevé des températures par date')
+    plt.xticks(rotation=45)
+    plt.grid(show_grid)
+    plt.show()
+
+os.remove("data_datetemp.json")
+print("File data_datetemp.json has removed")
+os.remove("data_temp.json")
+print("File data_temp.json has removed")
