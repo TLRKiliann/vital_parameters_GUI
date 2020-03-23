3248e01135a21b4090b6c405ba3f2139d4e9cfdb
TLRKiliann <philogenie@protonmail.com>
2020-03-23 19:46:56 +0100

    aspifile/aspidlr.py modified

--- aspifile/aspidlr.py
+++ aspifile/aspidlr.py
@@ -7,7 +7,7 @@ import json
 import time
 
 
-file = open("dlr.json")
+file = open('./aspifile/dlr.json')
 data = json.load(file)
 #file.close
 
@@ -26,7 +26,7 @@ for (key, value) in data.items():
     print("Date: " + str(value[1]["Date"]))
     print("Douleurs: " + str(value[1]["Douleurs"]))
     
-print("\nDay LOOP\n")
+print("\nList of dates\n")
 
 data_list1 = []
 for value in zip(value):
@@ -35,14 +35,14 @@ for value in zip(value):
 
 print("\nThat seems ok!\n")
 
-with open("data_datedlr.json", 'a+') as datafile:
+with open('./aspifile/data_datedlr.json', 'a+') as datafile:
     json.dump(data_list1, datafile, indent=4)
 
 for (key, value) in data.items():
     print(key, value)
     print("\n")
 
-print("\nPain LOOP\n")
+print("\nList of pain\n")
 
 data_list2 = []
 for value in zip(value):
@@ -51,12 +51,12 @@ for value in zip(value):
 
 print("\nThat seems correct!\n")
 
-with open("data_dlr.json", 'a+') as datafile:
+with open('./aspifile/data_dlr.json', 'a+') as datafile:
     json.dump(data_list2, datafile, indent=4)
 
-print("\nLancement du programme 'plot_dlr.py'...")
+print("\nDownloading 'plot_prog.py'...")
 # Un temps d'attente de 2 sec:
-print("Chargement dans 2 secondes...")
+print("Time wait 2 seconds...")
 time.sleep(2)
 # Lancement du programme plot_dlr.py
-os.system('python3 plot_dlr.py')
+os.system('./aspifile/plot/plot_dlr.py')
