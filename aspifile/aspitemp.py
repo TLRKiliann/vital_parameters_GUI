2fba8833cbe780b0d8156221c12930d644a557d1
TLRKiliann <philogenie@protonmail.com>
2020-03-23 19:48:18 +0100

    aspifile/aspitemp.py modified

--- aspifile/aspitemp.py
+++ aspifile/aspitemp.py
@@ -7,7 +7,7 @@ import json
 import time
 
 
-file = open("temp.json")
+file = open('./aspifile/temp.json')
 data = json.load(file)
 #file.close
 
@@ -26,7 +26,7 @@ for (key, value) in data.items():
     print("Date: " + str(value[1]["Date"]))
     print("Temperature: " + str(value[1]["Temperature"]))
     
-print("\nDay LOOP\n")
+print("\nList of dates\n")
 
 data_list1 = []
 for value in zip(value):
@@ -35,14 +35,14 @@ for value in zip(value):
 
 print("\nThat seems ok!\n")
 
-with open("data_datetemp.json", 'a+') as datafile:
+with open('./aspifile/data_datetemp.json', 'a+') as datafile:
     json.dump(data_list1, datafile, indent=4)
 
 for (key, value) in data.items():
     print(key, value)
     print("\n")
 
-print("\nTemperature LOOP\n")
+print("\nList of temp\n")
 
 data_list2 = []
 for value in zip(value):
@@ -51,12 +51,12 @@ for value in zip(value):
 
 print("\nThat seems correct!\n")
 
-with open("data_temp.json", 'a+') as datafile:
+with open('./aspifile/data_temp.json', 'a+') as datafile:
     json.dump(data_list2, datafile, indent=4)
 
-print("\nLancement du programme 'plot_temp.py'...")
+print("\nDownloading 'plot_prog.py'...")
 # Un temps d'attente de 2 sec:
-print("Chargement dans 2 secondes...")
+print("Time wait 2 seconds...")
 time.sleep(2)
 # Lancement du programme plot_temp.py
-os.system('python3 plot_temp.py')
+os.system('./aspifile/plot/plot_temp.py')
