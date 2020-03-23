2ac22c54daff477f7e68dee0f7340f898c9a851b
TLRKiliann <philogenie@protonmail.com>
2020-03-23 19:47:44 +0100

    aspifile/aspigly.py modified

--- aspifile/aspigly.py
+++ aspifile/aspigly.py
@@ -7,7 +7,7 @@ import json
 import time
 
 
-file = open("gly.json")
+file = open('./aspifile/gly.json')
 data = json.load(file)
 #file.close
 
@@ -26,7 +26,7 @@ for (key, value) in data.items():
     print("Date: " + str(value[1]["Date"]))
     print("Glycemie: " + str(value[1]["Glycemie"]))
     
-print("\nDay LOOP\n")
+print("\nList of dates\n")
 
 data_list1 = []
 for value in zip(value):
@@ -35,14 +35,14 @@ for value in zip(value):
 
 print("\nThat seems ok!\n")
 
-with open("data_dategly.json", 'a+') as datafile:
+with open('./aspifile/data_dategly.json', 'a+') as datafile:
     json.dump(data_list1, datafile, indent=4)
 
 for (key, value) in data.items():
     print(key, value)
     print("\n")
 
-print("\nGlycemia LOOP\n")
+print("\nList of glycemia\n")
 
 data_list2 = []
 for value in zip(value):
@@ -51,12 +51,12 @@ for value in zip(value):
 
 print("\nThat seems correct!\n")
 
-with open("data_gly.json", 'a+') as datafile:
+with open('./aspifile/data_gly.json', 'a+') as datafile:
     json.dump(data_list2, datafile, indent=4)
 
-print("\nLancement du programme 'plot_gly.py'...")
+print("\nDownloading 'plot_prog.py'...")
 # Un temps d'attente de 2 sec:
-print("Chargement dans 2 secondes...")
+print("Time wait 2 seconds...")
 time.sleep(2)
 # Lancement du programme plot_gly.py
-os.system('python3 plot_gly.py')
+os.system('./aspifile/plot/plot_gly.py')
