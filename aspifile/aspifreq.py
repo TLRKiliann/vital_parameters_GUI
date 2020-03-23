2d7a1fb133479cfff8251e8a5b1bb337f6f014c5
TLRKiliann <philogenie@protonmail.com>
2020-03-23 19:47:18 +0100

    aspifile/aspifreq.py modified

--- aspifile/aspifreq.py
+++ aspifile/aspifreq.py
@@ -7,7 +7,7 @@ import json
 import time
 
 
-file = open("freq.json")
+file = open('./aspifile/freq.json')
 data = json.load(file)
 #file.close
 
@@ -26,7 +26,7 @@ for (key, value) in data.items():
     print("Date: " + str(value[1]["Date"]))
     print("FR: " + str(value[1]["FR"]))
     
-print("\nDay LOOP\n")
+print("\nList of dates\n")
 
 data_list1 = []
 for value in zip(value):
@@ -35,14 +35,14 @@ for value in zip(value):
 
 print("\nThat seems ok!\n")
 
-with open("data_datefr.json", 'a+') as datafile:
+with open('./aspifile/data_datefr.json', 'a+') as datafile:
     json.dump(data_list1, datafile, indent=4)
 
 for (key, value) in data.items():
     print(key, value)
     print("\n")
 
-print("\nFR LOOP\n")
+print("\nList of freq\n")
 
 data_list2 = []
 for value in zip(value):
@@ -51,12 +51,12 @@ for value in zip(value):
 
 print("\nThat seems correct!\n")
 
-with open("data_fr.json", 'a+') as datafile:
+with open('./aspifile/data_fr.json', 'a+') as datafile:
     json.dump(data_list2, datafile, indent=4)
 
-print("\nLancement du programme 'plot_freq.py'...")
+print("\nDownloading 'plot_prog.py'...")
 # Un temps d'attente de 2 sec:
-print("Chargement dans 2 secondes...")
+print("Time wait 2 seconds...")
 time.sleep(2)
 # Lancement du programme plot_freq.py
-os.system('python3 plot_freq.py')
+os.system('./aspifile/plot/plot_freq.py')
